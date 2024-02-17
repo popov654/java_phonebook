3793de5bec31fc5d1e0ff0eb094ce2a9b0d326e3
popov654 <popov654@yandex.ru>
2024-02-18 00:11:51 +0300

    Refactoring

--- src/phonebook/client/ConsoleClient.java
+++ src/phonebook/client/ConsoleClient.java
@@ -24,55 +24,58 @@ public class ConsoleClient implements Runnable {
             while (true) {
                 System.out.print("Enter your command (\"get\", \"put\", \"find\", \"exists\", \"service\") or \"quit\" to exit: > ");
                 String line = reader.readLine();
-                String[] words = line.split("\\s+");
-                String command = words[0].toLowerCase();
-                switch (command) {
-                    case "get":
-                        if (words.length > 1) {
-                            System.out.println(server.getNumberByName(words[1]));
-                        } else {
-                            System.err.println("Invalid command. Usage: get [name]");
-                        }
-                        break;
-                        
-                    case "find":
-                        if (words.length > 1) {
-                            Optional<String> number = server.findNumberByName(words[1]);
-                            System.out.println(number.orElse("Not found"));
-                        } else {
-                            System.err.println("Invalid command. Usage: find [name]");
-                        }
-                        break;    
-                        
-                    case "put":
-                        if (words.length > 2) {
-                            server.putNumber(words[1].trim(), words[2].trim());
-                        } else {
-                            System.err.println("Invalid command. Usage: put [name] [number]");
-                        }
-                        System.out.println("Number was saved successfully");
-                        break;
-                    case "exists":
-                        if (words.length > 1) {
-                            System.out.println(server.numberExists(words[1].trim()) ? "yes" : "no");
-                        } else {
-                            System.err.println("Invalid command. Usage: exists [name]");
-                        }
-                        break;
-                    case "service":
-                        String number = server.getServiceContact();
-                        System.out.println(number != null ? number : "Not found");
-                        break; 
-                    case "quit":
-                        return;
-                }
+                executeCommand(line);
             }
-            
-        } catch (DataNotFoundException e1) {
-            System.err.println(e1.getMessage());
-        } catch (Exception e2) {
-            e2.printStackTrace();
+        } catch (DataNotFoundException e) {
+            System.err.println(e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
         } 
+    }
+    
+    private void executeCommand(String commandWithArgs) throws DataNotFoundException {
+        String[] words = commandWithArgs.split("\\s+");
+        String command = words[0].toLowerCase();
+        switch (command) {
+            case "get":
+                if (words.length > 1) {
+                    System.out.println(server.getNumberByName(words[1]));
+                } else {
+                    System.err.println("Invalid command. Usage: get [name]");
+                }
+                break;
+
+            case "find":
+                if (words.length > 1) {
+                    Optional<String> number = server.findNumberByName(words[1]);
+                    System.out.println(number.orElse("Not found"));
+                } else {
+                    System.err.println("Invalid command. Usage: find [name]");
+                }
+                break;    
+
+            case "put":
+                if (words.length > 2) {
+                    server.putNumber(words[1].trim(), words[2].trim());
+                } else {
+                    System.err.println("Invalid command. Usage: put [name] [number]");
+                }
+                System.out.println("Number was saved successfully");
+                break;
+            case "exists":
+                if (words.length > 1) {
+                    System.out.println(server.numberExists(words[1].trim()) ? "yes" : "no");
+                } else {
+                    System.err.println("Invalid command. Usage: exists [name]");
+                }
+                break;
+            case "service":
+                String number = server.getServiceContact();
+                System.out.println(number != null ? number : "Not found");
+                break; 
+            case "quit":
+                System.exit(0);
+        }
     }
     
 }
